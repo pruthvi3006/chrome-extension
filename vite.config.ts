067670855a8ec3b72303e5b99653689aa637ad80
parthjoshi19 <parthjoshi1973@gmail.com>
2025-06-12 22:16:05 +0530

    done neccessary changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'), // or your entry HTML
+      },
+      output: {
+        // important for browser extensions
+        assetFileNames: 'assets/[name].[ext]',
+        entryFileNames: '[name].js',
+      }
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
