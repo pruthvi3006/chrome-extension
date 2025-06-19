a04444a47f421933fe683970f831f3aa779890bc
Pruthviraj Kale <pruthviraj.kale23@pccoepune.org>
2025-06-19 19:49:20 +0530

    Delete src/web3AuthConfig.ts

--- src/web3AuthConfig.ts
+++
@@ -1,9 +0,0 @@
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-
-export const clientId = "BCU6qYdYUad5TLmmJwLE3k4TGml3cVUgQRAuGcBmBpIKzQpw3pnr7DcxaKU5e5wGH_WXMFS5tj5wJBKuvuc2WkU"; // Replace with your Web3Auth Client ID
-
-export const chainConfig = {
-  chainNamespace: CHAIN_NAMESPACES.EIP155,
-  chainId: "0x1", // Ethereum mainnet
-  rpcTarget: "https://rpc.ankr.com/eth",
-};
